25f96332fd1f470df5d23274158235be44e95bb4
dundich <dundich1@gmail.com>
2016-10-05 18:51:31 +0300

    Fake

--- App/Ait.Pay.Web/Ait.Pay.IContract/Fake.cs
+++ App/Ait.Pay.Web/Ait.Pay.IContract/Fake.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Ait.Pay.IContract
+﻿namespace Ait.Pay.IContract
 {
     public class Fake
     {
