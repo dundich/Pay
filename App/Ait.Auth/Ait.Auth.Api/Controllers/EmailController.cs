9df661ecd726a7dc7e131e31fa50eeb22920f1af
dundich <dundich1@gmail.com>
2016-10-20 00:35:04 +0300

    email

--- App/Ait.Auth/Ait.Auth.Api/Controllers/EmailController.cs
+++ App/Ait.Auth/Ait.Auth.Api/Controllers/EmailController.cs
@@ -2,7 +2,6 @@
 using System.ComponentModel.DataAnnotations;
 using System.Net;
 using System.Net.Mail;
-using System.Threading.Tasks;
 using System.Web.Http;
 
 namespace Ait.Auth.Api.Controllers
@@ -26,7 +25,7 @@ namespace Ait.Auth.Api.Controllers
         [Authorize]
         [Route("Send")]
         //[ValidateAntiForgeryToken]
-        public async Task<IHttpActionResult> Send(EmailFormModel model)
+        public IHttpActionResult Send(EmailFormModel model)
         {
             if (!ModelState.IsValid)
                 return BadRequest(ModelState);
@@ -35,27 +34,30 @@ namespace Ait.Auth.Api.Controllers
 
             var body = "<p>Email From: {0} ({1})</p><p>Message:</p><p>{2}</p>";
             var message = new MailMessage();
-            message.To.Add(new MailAddress("xxx@gmail.com"));  // to
-            message.From = new MailAddress("xxx@gmail.com");  // sender
+            message.To.Add(new MailAddress("xxxxxx@mail.ru"));  // to
+            message.From = new MailAddress("xxxxxx@gmail.com");  // sender
             message.Subject = "Your email subject";
             message.Body = string.Format(body, model.FromName, model.FromEmail, model.Message);
             message.IsBodyHtml = true;
+
+            var credential = new NetworkCredential
+            {
+                UserName = "******@gmail.com",
+                Password = "*******"
+            };
             try
             {
-                using (var smtp = new SmtpClient())
+                using (var smtp = new SmtpClient()
                 {
-                    var credential = new NetworkCredential
-                    {
-                        UserName = "xxx@gmail.com",  // replace with valid value
-                        Password = "XXXX"  // replace with valid value
-                    };
-                    smtp.Credentials = credential;
-                    smtp.Host = "smtp.gmail.ru";
-                    smtp.Port = 587;
-                    smtp.EnableSsl = true;
-                    smtp.UseDefaultCredentials = false;
-                    smtp.DeliveryMethod = SmtpDeliveryMethod.Network;
-                    //smtp.Timeout = 3000;
+                    Credentials = credential,
+                    Host = "smtp.gmail.com",
+                    Port = 587,
+                    EnableSsl = true,
+                    //UseDefaultCredentials = false,
+                    DeliveryMethod = SmtpDeliveryMethod.Network
+                })
+                {
+
                     smtp.Send(message);
 
                     //await smtp.SendMailAsync(message);
