3187881ca48c38aee27f04b676468f3e41234a38
dundich <dundich1@gmail.com>
2016-11-10 16:51:54 +0300

    materialize

--- App/Ait.Pay.Web/Ait.Pay.IContract/PayHelper.cs
+++ App/Ait.Pay.Web/Ait.Pay.IContract/PayHelper.cs
@@ -2,29 +2,9 @@
 {
     public static class PayHelper
     {
-        //public static T SetPropValue<T>(this T p, string key, string value)
-        //    where T : PayIdValue
-        //{
-        //    if (p == null) return null;
-
-        //    if (p._extProps == null)
-        //        p._extProps = new ExtProps { };
-        //    p._extProps[key] = value;
-        //    return p;
-        //}
-
-        //public static string GetPropValue(this PayIdValue p, string key)
-        //{
-        //    if (p == null || p._extProps == null) return null;
-        //    string value;
-        //    p._extProps.TryGetValue(key, out value);
-        //    return value;
-        //}
-
         public static PayIdValue PayValue(this string key, string value)
         {
             return new PayIdValue { Id = key, Value = value };
         }
-
     }
 }
